55d6e3f489cc66a7cd94343f5ce32a02812fb0c2
aces15224 <53838763+aces15224@users.noreply.github.com>
2019-12-15 22:26:24 -0600

    typo
    100%

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,5 +2,5 @@ USE burgers_db;
 
 INSERT INTO burgers (burger_name, devoured) VALUES ("Mushroom Burger", false);
 INSERT INTO burgers (burger_name, devoured) VALUES ("Spicy Burger", false);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Double Cheeseburger", false);;
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Cheeseburger", false);
 
