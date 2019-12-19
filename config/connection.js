a971096f526b471f774a36f4eceb476c84546dc2
aces15224 <53838763+aces15224@users.noreply.github.com>
2019-12-18 22:34:08 -0600

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,19 @@
+  
 const mysql = require("mysql");
-
 var connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
+}
+else {
   connection = mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_DBPORT,
-    user: process.env.MYSQL_USERNAME,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DB,
-    insecureAuth : true
-
-});
-} 
-
+    host: "localhost",
+    port: 3306,
+    user: "test",
+    password: "password",
+    database: "burgers_db"
+  });
+}
 connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
